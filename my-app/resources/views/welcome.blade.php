7da4c68f4d51703d9fa0b28429ccd2339b124549
Haiader <84993795+Haiader@users.noreply.github.com>
2026-04-07 21:44:00 +0300

    restrucur the proj

--- my-app/resources/views/welcome.blade.php
+++ my-app/resources/views/welcome.blade.php
@@ -1,10 +1,13 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-<title>Welcome</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
 </head>
 <body>
-<h1>Welcome to Laravel 12!</h1>
-<p>Hello, {{ $name }} </p>
+    <h1>{{ $name }}</h1>
+    <h2>{{ $age }}</h2>
 </body>
 </html>
--- my-app/routes/web.php
+++ my-app/routes/web.php
@@ -3,5 +3,14 @@
 use App\Http\Controllers\app;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/',          [app::class, 'index']);
-Route::post('/calculate', [app::class, 'calculate']);
+Route::get('/', function(){
+    return view('welcome', [
+        'name' => 'Haider',
+        'age' => 20
+    ]);
+});
+
+
+Route::get('/a',function(){
+    return view('calculator');
+});
